586958b3b65f4e0f0885f7e18fb73a90f0f2aea8
Sorooshm80 <139033249+Sorooshm80@users.noreply.github.com>
2024-03-26 12:14:48 +0330

    Add files via upload

---
+++ MainSource/Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Microsoft.Office.Interop.Word.Application objWord = new Microsoft.Office.Interop.Word.Application();
+        Microsoft.Office.Interop.Word.Document objDoc = new Microsoft.Office.Interop.Word.Document();
+
+        objWord.Options.ArabicNumeral = Microsoft.Office.Interop.Word.WdArabicNumeral.wdNumeralContext;
+        objDoc.Application.Options.ArabicNumeral = Microsoft.Office.Interop.Word.WdArabicNumeral.wdNumeralContext;
+        Microsoft.Office.Interop.Word.Range _range = objDoc.Range(Type.Missing, Type.Missing);
+
+    }
+}
